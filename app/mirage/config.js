071680d8e40cf0b2502cdb4a12c04c3442f1838d
Richard Shea <rshea@thecubagroup.com>
2016-03-23 13:57:29 +1300

    Removed hardcoded data from index route handler and make amendments so that the data is fetched from Mirage instead

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,4 +1,47 @@
+//This configures Mirage so that whenever Ember Data makes a GET 
+//request to /rentals, Mirage will return this JavaScript object as JSON.
+
 export default function() {
+  this.get('/rentals', function() {
+    return {
+      data: [{
+        type: 'rentals',
+        id: 1,
+        attributes: {
+          title: 'Grand Old Mansion',
+          owner: 'Veruca Salt',
+          city: 'San Francisco',
+          type: 'Estate',
+          bedrooms: 15,
+          image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+        }
+      }, {
+        type: 'rentals',
+        id: 2,
+        attributes: {
+          title: 'Urban Living',
+          owner: 'Mike Teavee',
+          city: 'Seattle',
+          type: 'Condo',
+          bedrooms: 1,
+          image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+        }
+      }, {
+        type: 'rentals',
+        id: 3,
+        attributes: {
+          title: 'Downtown Charm',
+          owner: 'Violet Beauregarde',
+          city: 'Portland',
+          type: 'Apartment',
+          bedrooms: 3,
+          image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
+        }
+      }]
+    };
+  });
+}
+//export default function() {
 
   // These comments are here to help you get started. Feel free to delete them.
 
@@ -72,7 +115,7 @@ export default function() {
     });
 
   */
-}
+//}
 
 /*
 You can optionally export a config that is only loaded during tests
